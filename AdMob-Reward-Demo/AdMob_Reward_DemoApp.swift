a713973c97997ca5e4e318425e08b6f4d7110424
SNQ-2001 <taishin05109@gmail.com>
2022-05-07 21:51:33 +0900

    Initial Commit

---
+++ AdMob-Reward-Demo/AdMob_Reward_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdMob_Reward_DemoApp.swift
+//  AdMob-Reward-Demo
+//
+//  Created by 宮本大新 on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct AdMob_Reward_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdMob-Reward-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdMob-Reward-Demo
+//
+//  Created by 宮本大新 on 2022/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
