5bb91cec78540f69655eed528fa5e38880d87df6
SNQ-2001 <taishin05109@gmail.com>
2022-05-08 03:45:43 +0900

    Update README

--- AdMob-Reward-Demo/AdMob_Reward_DemoApp.swift
+++ AdMob-Reward-Demo/AdMob_Reward_DemoApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import GoogleMobileAds
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
+    }
+}
 
 @main
 struct AdMob_Reward_DemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # AdMob-Reward-Demo
+## Official Documents
+[Get Started](https://developers.google.com/admob/ios/quick-start)
+
+[Rewarded ads](https://developers.google.com/admob/ios/rewarded)
+
+## Example
+![Videotogif](https://user-images.githubusercontent.com/84154073/167267647-6026a4ce-98d3-48f9-afaa-03b17944271e.gif)
